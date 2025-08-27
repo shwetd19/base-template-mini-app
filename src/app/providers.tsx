668bb87a6e4edc885b6414c5f6b8bd35bb282d4e
Shwetas Dhake <shwetasdhake16@gmail.com>
2025-08-27 14:05:33 +0530

    prod fix

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,6 +2,8 @@
 
 import dynamic from "next/dynamic";
 import { MiniAppProvider } from "@neynar/react";
+import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
+import { base } from "wagmi/chains";
 
 const WagmiProvider = dynamic(
   () => import("~/components/providers/WagmiProvider"),
@@ -13,7 +15,12 @@ const WagmiProvider = dynamic(
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <WagmiProvider>
-      <MiniAppProvider analyticsEnabled={true}>{children}</MiniAppProvider>
+      <MiniKitProvider 
+        apiKey={process.env.NEXT_PUBLIC_CDP_CLIENT_API_KEY || process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY} 
+        chain={base}
+      >
+        <MiniAppProvider analyticsEnabled={true}>{children}</MiniAppProvider>
+      </MiniKitProvider>
     </WagmiProvider>
   );
 }
